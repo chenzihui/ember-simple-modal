6b8a5611d36e2e5463faf3b6b420177f31230734
Chen Zihui <hello@chenzihui.com>
2013-02-01 13:20:05 +0800

    Add basic routing example

---
+++ assets/js/router.js
@@ -0,0 +1,18 @@
+/**
+ * An Ember.Router instance is automatically created when you create a new instance of Ember.Application
+ * as we have done so in `app.js` using the following:
+ *
+ * window.App = Ember.Application.create()
+ *
+ * This is where we define our routes, but unlike other MVC frameworks where controllers do most of the work
+ * involving state management, in Ember, the Router is responsible for maintaining application state.
+ **/
+
+App.Router.map(function() {
+  // This will load the 'about' template into the 'application' template's
+  // {{ outlet }}.
+  //
+  // Note that we didn't have to create an AboutController and / or AboutView
+  // for this to work. Ember automatically does this for us.
+  this.route( 'about' );
+});
